d05af27b6c671d1366d1689c2df682edeefef459
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-07-01 10:21:27 +0200

    Job03 done

---
+++ Jour06/Job03/Tab10.java
@@ -0,0 +1,15 @@
+package Jour06.Job03;
+
+public class Tab10 {
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        for (int i = 0; i < 10; i++) {
+            tab[i] = i;
+        }
+        System.out.println("tab[0] = " + tab[0]);
+        System.out.println("tab[1] = " + tab[1]);
+        System.out.println("tab[5] = " + tab[5]);
+        System.out.println("tab[9] = " + tab[9]);
+        System.out.println("tab[10] = " + tab[10]);
+    }
+}
