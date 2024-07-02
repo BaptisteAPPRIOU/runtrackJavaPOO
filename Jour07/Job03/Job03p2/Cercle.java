151da94fe4ee98d86f26d01c877892dc7920fb92
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-07-02 11:33:35 +0200

    Job03.2 done

---
+++ Jour07/Job03/Job03p2/Cercle.java
@@ -0,0 +1,47 @@
+package Jour07.Job03.Job03p2;
+
+public class Cercle {
+    protected double rayon;
+    protected double x;
+    protected double y;
+
+    public Cercle(double rayon, double x, double y) {
+        this.rayon = rayon;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void affiche() {
+        System.out.println("Rayon: " + this.rayon);
+        System.out.println("Centre: (" + this.x + ", " + this.y + ")");
+    }
+
+    public double getX() {
+        return this.x;
+    }
+    
+    public double getY() {
+        return this.y;
+    }
+
+    public void setCentre(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double surface() {
+        return Math.PI * Math.pow(this.rayon, 2);
+    }
+
+    public boolean estInterieur(double x, double y) {
+        return Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2) <= Math.pow(this.rayon, 2);
+    }
+
+    public double getRayon() {
+        return this.rayon;
+    }
+
+    public void setRayon(double rayon) {
+        this.rayon = rayon;
+    }
+}
---
+++ Jour07/Job03/Job03p2/Rectangle.java
@@ -0,0 +1,31 @@
+package Jour07.Job03.Job03p2;
+
+public class Rectangle {
+    protected double longueur;
+    protected double largeur;
+
+    public Rectangle(double longueur, double largeur) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+    public double getLongueur() {
+        return this.longueur;
+    }
+
+    public double getLargeur() {
+        return this.largeur;
+    }
+
+    public void setLongueur(double longueur) {
+        this.longueur = longueur;
+    }
+
+    public void setLargeur(double largeur) {
+        this.largeur = largeur;
+    }
+
+    public double surface() {
+        return this.longueur * this.largeur;
+    }
+}
