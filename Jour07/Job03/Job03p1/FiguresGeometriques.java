9f5f5f292687a471ec0635ea9cf8e935a1bfcafb
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-07-02 11:33:14 +0200

    Job03.1 done

---
+++ Jour07/Job03/Job03p1/FiguresGeometriques.java
@@ -0,0 +1,10 @@
+package Jour07.Job03.Job03p1;
+
+public class FiguresGeometriques {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("Surface du rectangle: " + rectangle.surface());
+        Cercle cercle = new Cercle(5, 0, 0);
+        cercle.affiche();
+    }
+}
